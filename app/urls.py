327776900ab14733ec15bac4b5a597dc46fdd1ce
Hilario Carreon <hilariocarreon03@gmail.com>
2025-04-25 19:08:31 -0300

    Agrega ruta para añadir calificaciones y reseñas a eventos

--- app/urls.py
+++ app/urls.py
@@ -13,4 +13,6 @@ urlpatterns = [
     path("events/<int:id>/edit/", views.event_form, name="event_edit"),
     path("events/<int:id>/", views.event_detail, name="event_detail"),
     path("events/<int:id>/delete/", views.event_delete, name="event_delete"),
+    path("events/<int:event_id>/rating/add/", views.add_rating, name="add_rating"),
 ]
+
