ca53e41cb190459a68ae8a089e397f394d0f03d3
jf-arce <josefranciscoarce21@gmail.com>
2025-05-02 00:17:28 -0300

    Refactor del navbar

--- app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from app.models import User, Event, Notification
-
-class AuthorAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(User, AuthorAdmin)
-admin.site.register(Event, AuthorAdmin)
-admin.site.register(Notification, AuthorAdmin)
--- eventhub/urls.py
+++ eventhub/urls.py
@@ -16,9 +16,7 @@ Including another URLconf
 """
 
 from django.urls import include, path
-from django.contrib import admin
 
 urlpatterns = [
     path("", include("app.urls")),
-    path("admin/", admin.site.urls),
 ]
