20f166100e801ba16ec50b2b89f725a066dc7bb1
JoaquinB3 <joaquinbotteri3@gmail.com>
2025-05-26 23:30:52 -0300

    Agrega metodo para filtrar eventos pasados al modelo event y tambien sus test unitarios

---
+++ app/test/test_unit/test_event_future.py
@@ -0,0 +1,15 @@
+import datetime
+
+from django.test import TestCase
+from django.utils import timezone
+from datetime import timedelta
+
+from app.models import Event
+
+class EventModelTest(TestCase):
+    
+    def test_event_is_future_is_scheduled_today_but_later(self):
+
+        future_time = timezone.now() + timedelta(hours=1)
+        event = Event(scheduled_at=future_time)
+        assert event.is_future() is True
