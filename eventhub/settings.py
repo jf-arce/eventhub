4f3ae7bbea14ad260654712c224cee97a3cadcfb
oriana-monaldi <orimonaldi@gmail.com>
2025-06-10 00:21:57 -0300

    Agrega csrf trusted origin

--- eventhub/settings.py
+++ eventhub/settings.py
@@ -32,6 +32,7 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
+CSRF_TRUSTED_ORIGINS = ["https://eventhub-643h.onrender.com",]
 
 # Application definition
 
