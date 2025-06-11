b7df50708672537dcfac2b65d1736e85a63534ae
jf-arce <josefranciscoarce21@gmail.com>
2025-06-11 19:30:28 -0300

    elimina csrf

--- eventhub/settings.py
+++ eventhub/settings.py
@@ -32,7 +32,7 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ["localhost,127.0.0.1,eventhub-643h.onrender.com"]
 
-CSRF_TRUSTED_ORIGINS = ["https://eventhub-643h.onrender.com"]
+# CSRF_TRUSTED_ORIGINS = ["https://eventhub-643h.onrender.com"]
 
 # Application definition
 
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
+    # "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
