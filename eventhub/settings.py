c71c0af4b0ad57dd4ca36244c65b40f99e366887
jf-arce <josefranciscoarce21@gmail.com>
2025-06-11 00:56:44 -0300

    Agrega dominio de render a la variable del settings ALLOWED_HOSTS

--- eventhub/settings.py
+++ eventhub/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure--f67ll=2-b2qolla9=1f8mtg@s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1,eventhub-643h.onrender.com").split(",")
 
 CSRF_TRUSTED_ORIGINS = ["https://eventhub-643h.onrender.com",]
 
